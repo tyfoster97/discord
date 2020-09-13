d7b1ebb549fa1c049d1f389753e187b43af130ee
Ty Foster <tyfoster@outlook.com>
2020-09-12 22:37:13 -0700

    Attempted fix

--- src/commands/fun/hug.js
+++ src/commands/fun/hug.js
@@ -1,7 +1,7 @@
 //gives adam a hug
 module.exports.run = async(client, message, args) => {
     try {
-        let huggedMember = await message.guild.member.cache.get(args);
+        let huggedMember = await message.guild.member.cache.get(args[0]);
         if(huggedMember) {
             console.channel.send("*hugs* " + huggedMember.tag);
         }
