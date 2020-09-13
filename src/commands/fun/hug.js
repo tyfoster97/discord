a0190891407a9d33c479a34893f68ae8030d7cf4
Ty Foster <tyfoster@outlook.com>
2020-09-12 22:25:12 -0700

    Implement hug for all users

---
+++ src/commands/fun/hug.js
@@ -0,0 +1,12 @@
+//gives adam a hug
+module.exports.run = async(client, message, args) => {
+    try {
+        let huggedMember = await message.guild.member.cache.get(args);
+        if(huggedMember) {
+            console.channel.send("*hugs* " + huggedMember.tag);
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
