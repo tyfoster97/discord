fc9af0d6e1d3551e0e5ccda0e3e0f0a29fffee85
Ty Foster <tyfoster@outlook.com>
2020-09-13 00:59:20 -0700

    Implement general help embed

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -1,14 +1,15 @@
 const discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    let embed = new discord.MessageEmbed();
+    
     let fields = {};
     if(args[0]) {
         console.log('oops');
     }
     else {
-        embed.setTitle('Help');
-        embed.setDescription('use /help <command> for help with specific commands\n\nd \nflipcoin \nattention \nhug \npotionseller \n');
+        let embed = new discord.MessageEmbed()
+        .setTitle('Help')
+        .setDescription('use /help <command> for help with specific commands\n\nd \nflipcoin \nattention \nhug \npotionseller \n');
+        message.channel.send(embed);
     }
-    message.channel.send(embed);
 }
