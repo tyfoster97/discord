f0367e4d525015ae46d20d679882e160e77c82b3
Ty Foster <tyfoster@outlook.com>
2020-09-13 00:27:43 -0700

    Fix scope of embed

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -1,44 +1,44 @@
 const discord = require("discord.js")
-module.exports.run = (client, message, cmd) => {
-    let embed = new discord.MessageEmbed();
+const embed = new discord.MessageEmbed();
+module.exports.run = (client, message, args) => {
     embed.setTitle(args);
+    embed = getInfo(embed, args);
+    message.channel.send(embed);
+}
+
+const getInfo = (embed, cmd) => {
     if (cmd === 'd') {
         embed.addField('Description', 'rolls a die with a user-specified number of sides');
-        embed.addField('Use', '\/d \<number\>');
+        embed.addField('Use', '/d <number>');
         embed.addField('Notes', 'Can only roll up to d100');
     }
     else if (cmd === 'flipcoin') {
         embed.addField('Description', 'filps a coin');
-        embed.addField('Use', '\/flipcoin');
+        embed.addField('Use', '/flipcoin');
     }
     else if (cmd === 'hug') {
         embed.addField('Description', 'hugs a member');
-        embed.addField('Use', '\/hug \<\@member\>');
+        embed.addField('Use', '/hug <@member>');
         embed.addField('Notes', 'tag the member');
     }
     else if (cmd === 'attention') {
         embed.addField('Description', 'gives the user attention');
-        embed.addField('Use', '\/attention');
+        embed.addField('Use', '/attention');
     }
     else if (cmd === 'potionseller') {
         embed.addField('Description', 'returns 3 iconic youtube videos');
-        embed.addField('Use', '\/potionseller');
+        embed.addField('Use', '/potionseller');
     }
     else if (cmd === 'fratx') {
         embed.addField('Description', 'returns the titles of 6 XXX videos');
-        embed.addField('Use', '\/fratx');
+        embed.addField('Use', '/fratx');
     }
     else {
-        embed.addField('Info', 'use \?\<command\-name\> for more info:');
+        embed.addField('Info', 'use \?<command-name> for more info:');
         embed.addField('flipcoin', '');
         embed.addField('d','');
         embed.addField('hug', '');
         embed.addField('attention', '');
         embed.addField('potionseller', '');
     }
-    message.channel.send(embed);
-}
-
-const getInfo = (embed, cmd) => {
-    
 }
