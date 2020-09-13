ab1db53e8bfd2cdf82bb48a92b7745fab219426a
Ty Foster <tyfoster@outlook.com>
2020-09-13 05:10:06 +0100

    Remove duplicate file

--- bot.js
+++
@@ -1,5 +0,0 @@
-const discord = require('discord.js'); //discord dependency
-const client = new discord.Client(); //discord client
-require('dotenv').config();
-
-client.login(process.env.BOT_TOKEN); //use bot token from env file
