c42ea1fc81396e17c6886bfa9be3749d41297bea
Ty Foster <tyfoster@outlook.com>
2020-09-12 18:11:16 -0700

    Load commands from files

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
+const fs = require('fs').promises;
+const path = require('path');
 const discord = require('discord.js'); //discord dependency
 const client = new discord.Client(); //discord client
 PREFIX = process.env.PREFIX;
 ADMIN_PREFIX = process.env.ADMIN_PREFIX;
-
 client.login(process.env.BOT_TOKEN); //use bot token from env file
-
+client.commands = new Map();
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged on`);
 });
+
 //check if command is in a valid format
 const isCmd = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName);
 //check if command is a valid admin level command
@@ -53,6 +55,25 @@ client.on('message', function(message) {
     }
     
 });
+
+(async function registerCommands(dir = 'commands') {
+    //read directory
+    let files = await fs.readdir(path.join(__dirname, dir));
+    console.log(files);
+    //loop through files
+    for(let file of files) {
+        let stat = await fs.lstat(path.join(__dirname, dir, file));
+        if(stat.isDirectory())
+            registerModels(path.join(dir, file));
+        else {
+            if(file.endsWith(".js")) {
+                let cmdName = file.substring(0, file.indexOf(".js"));
+                let cmdModule = require(path.join(__dirname, dir, file));
+                client.commands.set(cmdName, cmdModule);
+            }
+        }
+    }
+})()
 
 //checks if user is 18+ and allows access to nsfw channels
 function check18Plus(message) {
