91121ab33b302fada305a887edbf2ae1bdb41715
Ty Foster <tyfoster@outlook.com>
2020-09-12 22:43:18 -0700

    Fix bug where message printed to console instead of channel

--- src/commands/fun/hug.js
+++ src/commands/fun/hug.js
@@ -1,9 +1,9 @@
 //gives adam a hug
 module.exports.run = async(client, message, args) => {
     try {
-        let huggedMember = await message.guild.member.cache.get(args[0]);
+        let huggedMember = await message.guild.member.cache.get(args);
         if(huggedMember) {
-            console.channel.send("*hugs* " + huggedMember.tag);
+            message.channel.send("*hugs* " + huggedMember.tag);
         }
     }
     catch(err) {
