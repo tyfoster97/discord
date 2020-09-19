dcf812e6b35fecb691fc8c91cd7f1a7ed4ca6c49
Ty Foster <tyfoster@outlook.com>
2020-09-19 02:38:14 -0700

    Change variable names

--- src/build-test.js
+++ src/build-test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 fs.readFile('src/bot.js', (err, srcCode) => {
   if (err) throw err;
   var srcString = srcCode.toString();//get source code
-  srcString = srcString.replace('bot_token', 'test_token');//replace bot tokens
+  srcString = srcString.replace('BOT_TOKEN', 'TEST_TOKEN');//replace bot tokens
   fs.writeFile('src/test.js', srcString, error => {
     if (error) throw error;
   });//write test file
