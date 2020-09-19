10b481b10e9b70d68af94b7539e5e9cbe2b65516
Ty Foster <tyfoster@outlook.com>
2020-09-19 01:48:58 -0700

    Add comments

--- src/build-test.js
+++ src/build-test.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 fs.readFile('src/bot.js', (err, srcCode) => {
   if (err) throw err;
-  var srcString = srcCode.toString();
-  srcString = srcString.replace('bot_token', 'test_token');
+  var srcString = srcCode.toString();//get source code
+  srcString = srcString.replace('bot_token', 'test_token');//replace bot tokens
   fs.writeFile('src/test.js', srcString, error => {
     if (error) throw error;
-  });
+  });//write test file
 });
