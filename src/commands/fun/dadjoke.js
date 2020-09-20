ff7c07081177d5fdabd1e944bd7d91c94e35be0c
Ty Foster <tyfoster@outlook.com>
2020-09-19 22:42:30 -0700

    Fix typo

--- src/commands/fun/dadjoke.js
+++ src/commands/fun/dadjoke.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, arg) => {
     phrase = message.content.substring(message.content.indexOf(' ') + 1);
   } else if (arg == 3) {
     //if message starts with i am
-    phrase = messafe.content.substring(message.content.indexOf(' ', 2) + 1);
+    phrase = message.content.substring(message.content.indexOf(' ', 2) + 1);
   }
 
   message.channel.send('Hi, ' + phrase + ' I\'m _mombot!');
