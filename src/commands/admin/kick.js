ef1b71ad5f37e7a3d4d52d2eb95b6f523f3a9973
Ty Foster <tyfoster@outlook.com>
2020-09-21 00:11:04 -0700

    Implement kick command
    allows mods to kick users with _mombot
    adds .env variable for bot color
    updates mentions of color throughout existing code

--- src/commands/admin/kick.js
+++ src/commands/admin/kick.js
@@ -1,52 +1,51 @@
-const { MessageEmbed, Permissions } = require("discord.js");
+const { MessageEmbed, Permissions } = require('discord.js');
 const botColor = process.env.COLOR;
 
 function getReason(args) {
-    let rsn = 'They were bad';
-    //if a reason was given use that instead
+    let rsn = 'They did a bad thing, but not bad enough to ban';
+    //if a reason is given use that instead
     if (args[0]) {
         rsn = args.join(' ');
     }
     return rsn;
-}
+};
 
-//ban member
-function ban(member, args) {
+function kick(member, args) {
     member
-    .ban({
-        reason: getReason(args),
+    .kick({
+        reason: getReason(args)
     })
     .catch(err => {
         console.log(err);
         return false;
-    });
+    })
     return true;
 };
 
-/* Handles command to ban a user */
+/* Handles kick command */
 module.exports.run = async (client, message, args) => {
-    //if author has permission to ban members
-    if (message.author.flags.has(Permissions.BAN_MEMBERS)) {
-        const user = message.mentions.users.first();
+    //if author has permission to kick users
+    if (message.author.flags.has(Permissions.KICK_MEMEBERS)) {
+        const user = message.mentions.user.first();
         //if there is a mentioned user
         if (user) {
             const member = message.guild.member(user);
             //if the member exists
             if (member) {
-                args.shift(); //pop user name off of args
-                //if ban was successful
-                if (ban(member, args)) {
+                args.shift();
+                //if kick was successful
+                if (kick(member, args)) {
                     const msg = new MessageEmbed()
                     .setColor(botColor)
-                    .setTitle('Member banned')
-                    .setDescription(`Successfully banned ${user.tag}`)
+                    .setTitle('Member kicked')
+                    .setDescription(`Successfully kicked ${user.tag}`)
                     .addField('Reason', getReason(args));
                     message.reply(msg);
                 } else {
                     const msg = new MessageEmbed()
                     .setColor(botColor)
-                    .setTitle('Unable to ban member')
-                    .setDescription(`Could not ban ${user.tag}`);
+                    .setTitle('Unable to kick member')
+                    .setDescription(`Could not kick ${user.tag}`);
                     let m = await message.reply(msg);
                     await m.delete({timeout: 5000})
                     .catch(err => console.log(err));
@@ -56,7 +55,7 @@ module.exports.run = async (client, message, args) => {
                 const msg = new MessageEmbed()
                 .setColor(botColor)
                 .setTitle('User not in server')
-                .setDescription('Command should ban user in the server');
+                .setDescription('Command should kick user in the server');
                 let m = await message.reply(msg);
                 await m.delete({timeout: 5000})
                 .catch(err => console.log(err));
@@ -66,9 +65,9 @@ module.exports.run = async (client, message, args) => {
             const msg = new MessageEmbed()
             .setColor(botColor)
             .setTitle('No user mentioned')
-            .setDescription('Command should be of the form /ban @<user> [reason]')
+            .setDescription('Command should be of the form /kick @<user> [reason]')
             .addFields({
-                name: 'Examples', value: '/ban @_mombot She\'s annoying\n/ban @_mombot'
+                name: 'Examples', value: '/kick @_mombot She posted NSFW content in a SFW channel\n/kick @_mombot'
             });
             let m = await message.reply(msg);
             await m.delete({timeout: 10000})
@@ -79,7 +78,7 @@ module.exports.run = async (client, message, args) => {
         const msg = new MessageEmbed()
         .setColor(botColor)
         .setTitle('Insufficient permissions')
-        .setDescription('You do not have permission to ban members');
+        .setDescription('You do not have permission to kick members');
         let m = await message.reply(msg);
         await m.delete({timeout: 5000})
         .catch(err => console.log(err));
